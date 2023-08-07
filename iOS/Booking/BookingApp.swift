7a16154675c5c7752ffd9ab77e439eca77cf5d16
Samuel <samuel.song.bc@gmail.com>
2023-08-07 22:03:12 +0700

    [iOS] init

---
+++ iOS/Booking/BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingApp.swift
+//  Booking
+//
+//  Created by Samuel on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
